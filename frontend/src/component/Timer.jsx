da82aa7a3016dab9d1602b8ac616e88f0fb4c3d1
krish <sharmageeta847@gmail.com>
2023-11-28 00:55:33 +0530

    project complete

--- frontend/src/component/Timer.jsx
+++ frontend/src/component/Timer.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-function Stopwatch() {
+function Timer() {
   const initialDuration = 5 * 60 * 1000; // 5 minutes in milliseconds
   const [time, setTime] = useState(initialDuration);
   const [isRunning, setIsRunning] = useState(false);
@@ -12,23 +13,20 @@ function Stopwatch() {
   useEffect(() => {
     let interval;
     let visibilityTimeout;
-   
 
     if (isRunning && time > 0) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime - 10); // Decrease by 10 milliseconds for more accuracy
+        setTime((prevTime) => prevTime - 10);
       }, 10);
-    } else if ( time <= 0 ) {
-      // If the timer has ended and remaining buttons are not yet shown, switch visibility
+    } else if (time <= 0) {
       setShowTimerBtn(true);
       setShowRemainingButtons(true);
       visibilityTimeout = setTimeout(() => {
         setShowTimerBtn(false);
-        setShowRemainingButtons(false); // Reset to false after timeout
+        setShowRemainingButtons(false);
         setTime(initialDuration);
-        
         setIsRunning(true);
-      }, 5000); 
+      }, 5000);
     }
 
     return () => {
@@ -60,28 +58,11 @@ function Stopwatch() {
 
   const fetchDeletedTeams = async () => {
     try {
-      const response = await axios.get('https://coderush.onrender.com/api/deleted');
+      const response = await axios.get(`${BASE_URL}/api/deleted`);
       setDeletedTeams(response.data);
     } catch (error) {
       console.error('Error fetching deleted teams data:', error);
     }
-  };
-
-  const deleteToggle = () => {
-    axios
-      .post('https://coderush.onrender.com/api/removeteam', {
-        time: '1',
-        team: '1',
-      })
-      .then((response) => {
-        // Handle the response if needed
-        setIsRunning(true);
-        setShowTimerBtn(true);
-        setShowRemainingButtons(false);
-      })
-      .catch((error) => {
-        console.error('Error making POST request:', error);
-      });
   };
 
   useEffect(() => {
@@ -91,42 +72,65 @@ function Stopwatch() {
     // Set up interval to fetch data every 30 seconds
     const fetchIntervalId = setInterval(() => {
       fetchDeletedTeams();
-    }, 30000);
-
-    // Set up interval to perform deleteToggle every 4.5 minutes (4.5 * 60 * 1000 milliseconds)
-    const deleteToggleIntervalId = setInterval(() => {
-      deleteToggle();
-    }, 4.5 * 60 * 1000);
+    }, 1000);
 
     // Clear intervals on component unmount
     return () => {
       clearInterval(fetchIntervalId);
-      clearInterval(deleteToggleIntervalId);
     };
   }, []); // Empty dependency array to run only once on mount
 
+  const targetHours = 0;
+  const targetMinutes = 48;
 
+  // ... (rest of your component code)
+
+  // Custom target time
+  useEffect(() => {
+    console.log('Setting up custom target time effect');
+    
+    const now = new Date();
+    const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), targetHours, targetMinutes, 0, 0);
+  
+    console.log('Current time:', now);
+    console.log('Target time:', targetTime);
+  
+    const timeUntilTarget = targetTime - now;
+    console.log('Time until target:', timeUntilTarget);
+  
+    if (timeUntilTarget > 0 && timeUntilTarget <= 10000) {
+      console.log('Triggering handleToggle immediately');
+      // If within 10 seconds of the target time, trigger the toggle immediately
+      handleToggle();
+    }
+  
+    // Set up timeout to trigger the toggle when within 10 seconds of the target time
+    const timeoutId = setTimeout(() => {
+      console.log('Triggering handleToggle after timeout');
+      handleToggle();
+    }, timeUntilTarget - 10000); // Set timeout 10 seconds before the target time
+  
+    return () => {
+      console.log('Cleaning up custom target time effect');
+      clearTimeout(timeoutId);
+    };
+  }, []);
 
   const sortedDeletedTeam = [...deletedTeams].sort((a, b) => b.score - a.score);
 
-
-  const handleDeleteAndToggle = () => {
-    deleteToggle();
-    handleToggle();
-  };
-
   return (
     <div className='w-full flex mt-2  pb-4 gap-28 justify-center'>
-      <button className='border w-52 text-secondary text-3xl bg-primary px-3 py-2 rounded-md ' onClick={handleDeleteAndToggle}>
+      <button className='border w-52 text-secondary text-3xl bg-primary px-3 py-2 rounded-md ' onClick={handleToggle}>
         {isRunning ? formatTime(time) : 'Start Timer'}
       </button>
-       {showRemainingButtons && sortedDeletedTeam.slice(0, 3).map((team, index) => (
-        <button key={index} className='border w-52 text-secondary text-2xl bg-primary px-3 py-2 rounded-md '>
-          {team.hacker}
-        </button>
-      ))}
+      {showRemainingButtons &&
+        sortedDeletedTeam.slice(0, 3).map((team, index) => (
+          <button key={index} className='border w-52 text-secondary text-2xl bg-primary px-3 py-2 rounded-md '>
+            {team.hacker}
+          </button>
+        ))}
     </div>
   );
 }
 
-export default Stopwatch;
+export default Timer;
