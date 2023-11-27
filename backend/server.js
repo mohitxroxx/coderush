224898a7904c78ce12ab70c5ca46b704f0af9975
Mohitxroxx <mohitsinghroxx@gmail.com>
2023-11-27 21:12:17 +0530

    fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const connectDB=require('./config/db')
 const routes=require('./routes/routes')
+const cors=require('cors')
 
 connectDB()
 const app=express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api',routes)
