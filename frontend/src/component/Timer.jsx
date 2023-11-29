286402fb88db250820c95cbffae98267ccdaf2ed
Mohitxroxx <mohitsinghroxx@gmail.com>
2023-11-29 19:42:33 +0530

    timer new

--- frontend/src/component/Timer.jsx
+++ frontend/src/component/Timer.jsx
@@ -92,7 +92,7 @@ function Timer() {
   }, []); // Empty dependency array to run only once on mount
 
   const targetHours = 19;
-  const targetMinutes =40;
+  const targetMinutes =45;
 
   // ... (rest of your component code
 
